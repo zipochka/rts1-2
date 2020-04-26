c1a03a48fe9d04b503631807faf75d5b32a75329
zipochka <56541763+zipochka@users.noreply.github.com>
2020-04-26 21:29:40 +0300

    rts1.1,1.2
    доп. задание

--- rts1.py
+++ rts1.py
@@ -1,59 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 #import seaborn as sns
 
 n = 8
 omega = 1100
-N = 256
+rezXX=[]
+rezXY=[]
+for N in range (2,512):
+    x = np.zeros(N)
 
-x = np.zeros(N)
+    for i in range(1, n + 1):
+        A = np.random.random()
+        phi = np.random.random()
 
-for i in range(1, n + 1):
-    A = np.random.random()
-    phi = np.random.random()
+        for t in range(N):
+            x[t] += A * np.sin(omega / i * (t + 1) + phi)
 
-    for t in range(N):
-        x[t] += A * np.sin(omega / i * (t + 1) + phi)
+    Mx = np.sum(x) / N
+    Dx = np.sum((x - Mx) ** 2) / (N - 1)
 
-plt.figure(figsize=(8, 4))
-plt.title("Pseudo-random variable")
-plt.plot(range(N), x, "r")
-# plt.scatter(range(N), x) plt.show()
+    n_R = N // 2 - 1
+    start=time.time()
+    Rxx = np.zeros(n_R)
+    for tau in range(N // 2 - 1):
+        for t in range(N // 2 - 1):
+              Rxx[tau] += (x[t] - Mx) * (x[t + tau] - Mx) / (N - 1)
+    end=time.time()+1
+    rezXX.append(end-start)
 
-Mx = np.sum(x) / N
-Dx = np.sum((x - Mx) ** 2) / (N - 1)
+    y = np.zeros(N)
 
-plt.figure(figsize=(8, 4))
-plt.title("Pseudo-random variable")
-plt.plot(range(N), x, "r")  # random variable plt.plot(range(N), [Mx,]*N, "g")	# Mx
-plt.plot(range(N), [Mx + np.sqrt(Dx), ] * N, "b")  # Upper sqrt(Dx)
-plt.plot(range(N), [Mx - np.sqrt(Dx), ] * N, "b")  # Lower sqrt(Dx) plt.show()
+    for i in range(1, n + 1):
+    # a, phi = a_phi[i-1]
+        a, phi = np.random.random(), np.random.random()
+        for t in range(N):
+            y[t] += a * np.sin(omega / i * (t + 1) + phi)
 
-n_R = N // 2 - 1
-R = np.zeros(n_R)
-for tau in range(N // 2 - 1):
-    for t in range(N // 2 - 1):
-          R[tau] += (x[t] - Mx) * (x[t + tau] - Mx) / (N - 1)
+    n_R = N // 2 - 1
+    start=time.time()
+    Rxy = np.zeros(n_R)
+    for tau in range(N // 2 - 1):
+        for t in range(N // 2 - 1):
+            Rxy[tau] += (x[t] - Mx) * (y[t + tau] - Mx) / (N - 1)
+    end=time.time()+1
+    rezXY.append(end-start)
 
-plt.figure(figsize=(8, 4))
-plt.title("Autocorrelation function")
-plt.plot(range(n_R), R, "r")  # random variable plt.show()
+rez=[]
+for i in range(len(rezXX)):
+    rez.append( rezXX[i]*100/rezXY[i])
 
-y = np.zeros(N)
-
-for i in range(1, n + 1):
-# a, phi = a_phi[i-1]
-    a, phi = np.random.random(), np.random.random()
-    for t in range(N):
-        y[t] += a * np.sin(omega / i * (t + 1) + phi)
-
-n_R = N // 2 - 1
-Rxy = np.zeros(n_R)
-for tau in range(N // 2 - 1):
-    for t in range(N // 2 - 1):
-        Rxy[tau] += (x[t] - Mx) * (y[t + tau] - Mx) / (N - 1)
-
-plt.figure(figsize=(8, 4))
-plt.title("Autocorrelation function")
-plt.plot(range(n_R), Rxy, "r")  # random variable
+plt.figure()
+plt.title("rez")
+plt.plot(range(2,512),rez)
 plt.show()
+
+
